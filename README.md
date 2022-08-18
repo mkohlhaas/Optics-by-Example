efbb948b7f5e9989bc54415c1e7ecd149b21878e
Hans-J. Schmid <knock@myopendoor.de>
2022-08-18 11:07:42 +0200

    initializes repository.

---
+++ README.md
@@ -0,0 +1,1 @@
+[Exercises for `Optics by Example` by Chris Penner](https://leanpub.com/optics-by-example)
---
+++ app/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import Control.Lens
+import Control.Applicative
+import Data.Char
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified Data.Text as T
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
