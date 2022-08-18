f1bc2c30a06aaf66ba648c79792c950c221c5626
Hans-J. Schmid <knock@myopendoor.de>
2022-08-18 20:48:05 +0200

    starts ch. 3.

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,301 @@
 module Main where
 
-import Control.Lens
 import Control.Applicative
+import Control.Lens
+import Control.Lens.Extras (biplate)
+import Control.Lens.Unsound (lensProduct)
+import Data.Bits.Lens (bitAt)
 import Data.Char
+import Data.List (sort)
+import qualified Data.List as L
 import qualified Data.Map as M
 import qualified Data.Set as S
 import qualified Data.Text as T
+import Numeric.Lens (negated)
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main ∷ IO ()
+main = putStrLn "Hello Optics by Example!"
+
+-- |
+-- Update portions of immutable data structures
+-- >>> set _3 False ('a', 'b', 'c')
+-- ('a','b',False)
+
+-- |
+-- We can perform a task over deeply nested subsets of data.
+-- Let's sum all numbers which in a 'Left' within the right half of each tuple
+-- >>> sumOf (folded . _2 . _Left) [(True, Left 10), (False, Right "pepporoni"), (True, Left 20)]
+-- 30
+
+-- |
+-- >>> stories = ["This one time at band camp", "Nuff!", "This is a short story"]
+-- >>> over (traversed . filtered ((>10) . length)) (\story → take 10 story ++ "...") stories
+-- ["This one t...","Nuff!","This is a ..."]
+
+-- |
+-- Summarize a list of numbers, subtracting the 'Left's, adding the 'Right's!
+-- >>> sumOf (folded . beside negated id) [Left 1, Right 10, Left 2, Right 20]
+-- 27
+
+-- |
+-- >>> "why is a raven like a writing desk" & worded . _head %~ toUpper
+-- "Why Is A Raven Like A Writing Desk"
+
+-- |
+-- Multiply every Integer by 100 no matter where they are in the structure:
+-- >>> (Just 3, Left ("hello", [13, 15, 17])) & biplate *~ 100
+-- (Just 300,Left ("hello",[1300,1500,1700]))
+
+-- |
+-- Reverse the ordering of all even numbers in a sequence.
+-- We leave the odd numbers alone!
+-- >>> [1, 2, 3, 4, 5, 6, 7, 8] & partsOf (traversed . filtered even) %~ reverse
+-- [1,8,3,6,5,4,7,2]
+
+-- |
+-- Sort all the characters in all strings, across word boundaries!
+-- >>> ("one", "two", "three") & partsOf (each . traversed) %~ L.sort
+-- ("eee","hno","orttw")
+
+-- |
+-- Flip the 2nd bit of each number to a 0
+-- >>> [1 ∷ Int, 2, 3, 4] & traversed . bitAt 1 %~ not
+-- [3,0,1,6]
+
+-- Prompt the user with each question in a tuple, then return the tuple with each prompt replaced with the user's input.
+-- prompts = ("What is your name?" , "What is your quest?" , "What is your favourite color?")
+-- prompts & each %%~ (\prompt → putStrLn prompt >> getLine)
+
+-- |
+-- >>> view (_2 . _1) (42, ("hello", False))
+-- "hello"
+
+-- |
+-- >>> view (_1 . _2) ((1, 2), 3)
+-- 2
+
+-- |
+-- >>> set (_2 . _Left) "new" (False, Left "old")
+-- (False,Left "new")
+
+-- |
+-- >>> over (taking 2 worded . traversed) toUpper "testing one two three"
+-- "TESTING ONE two three"
+
+-- |
+-- >>> foldOf (both . each) (["super", "cali"],["fragilistic", "expialidocious"])
+-- "supercalifragilisticexpialidocious"
+
+-- |
+-- >>> view _1 ('a', 'b')
+-- 'a'
+
+-- |
+-- >>> view _2 ('a', 'b')
+-- 'b'
+
+-- |
+-- >>> set _1 'x' ('a', 'b')
+-- ('x','b')
+
+-- |
+-- >>> over _1 (*100) (1, 2)
+-- (100,2)
+
+-- |
+-- >>> view numCrew purplePearl
+-- 38
+
+-- |
+-- >>> _numCrew purplePearl
+-- 38
+
+----------
+-- Ship --
+----------
+data Ship = Ship
+  { _name ∷ String,
+    _numCrew ∷ Int
+  }
+  deriving (Show)
+
+purplePearl ∷ Ship
+purplePearl =
+  Ship
+    { _name = "Purple Pearl",
+      _numCrew = 38
+    }
+
+makeLenses ''Ship
+
+-- numCrew ∷ Lens' Ship Int
+-- numCrew = lens _numCrew (\ship newNumCrew → ship {_numCrew = newNumCrew})
+
+-- name ∷ Lens' Ship String
+-- name = lens _name (\ship newName → ship {_name = newName})
+
+-- |
+-- >>> purplePearl
+-- Ship {_name = "Purple Pearl", _numCrew = 38}
+
+-- |
+-- >>> set numCrew 41 purplePearl
+-- Ship {_name = "Purple Pearl", _numCrew = 41}
+
+-- |
+-- >>> set name "More Purple Pearl" purplePearl
+-- Ship {_name = "More Purple Pearl", _numCrew = 38}
+
+-- |
+-- >>> over numCrew (+3) purplePearl
+-- Ship {_name = "Purple Pearl", _numCrew = 41}
+
+-- |
+-- >>> set numCrew (view numCrew purplePearl + 3) purplePearl
+-- Ship {_name = "Purple Pearl", _numCrew = 41}
+
+-- |
+-- 1st lens law:
+-- You get back what you set (set-get)
+-- >>> let newValue = "Firefly"
+-- >>> view _1 (set _1 newValue ("Star Wars", "Star Trek"))
+-- "Firefly"
+
+-- |
+-- >>> let newValue = "Firefly"
+-- >>> view _1 (set _1 newValue ("Star Wars", "Star Trek")) == newValue
+-- True
+
+-- |
+-- 2nd lens law:
+-- Setting back what you got doesn’t do anything (get-set)
+-- >>> let structure = ("Simpsons", "Seinfeld")
+-- >>> set _1 (view _1 structure) structure
+-- ("Simpsons","Seinfeld")
+
+-- |
+-- >>> let structure = ("Simpsons", "Seinfeld")
+-- >>> set _1 (view _1 structure) structure == structure
+-- True
+
+-- |
+-- 3rd lens law:
+-- Setting twice is the same as setting once (set-set)
+-- >>> let value1 = "Coffee"
+-- >>> let value2 = "Red Bull"
+-- >>> let structure = ("Beer", "Tea")
+-- >>> set _1 value2 (set _1 value1 structure)
+-- ("Red Bull","Tea")
+
+-- |
+-- >>> let value1 = "Coffee"
+-- >>> let value2 = "Red Bull"
+-- >>> let structure = ("Beer", "Tea")
+-- >>> set _1 value2 structure
+-- ("Red Bull","Tea")
+
+-- |
+-- >>> let value1 = "Coffee"
+-- >>> let value2 = "Red Bull"
+-- >>> let structure = ("Beer", "Tea")
+-- >>> set _1 value2 (set _1 value1 structure) == set _1 value2 structure
+-- True
+
+---------
+-- Err --
+---------
+data Err
+  = ReallyBadError {_msg ∷ String}
+  | ExitCode {_code ∷ Int}
+
+makeLenses ''Err
+
+-- msg ∷ Lens' Err String
+-- msg = lens getMsg setMsg
+--   where
+--     getMsg (ReallyBadError message) = message
+--     getMsg (ExitCode _) = "" -- Hrmm, I guess we just return ""?
+--     setMsg (ReallyBadError _) newMessage = ReallyBadError newMessage
+--     setMsg (ExitCode n) newMessage = ExitCode n -- Nowhere to set it, I guess we do nothing?
+
+-- |
+-- >>> let newMessage = "False alarm!"
+-- >>> view msg (set msg newMessage (ReallyBadError "BAD BAD BAD"))
+-- "False alarm!"
+
+-- |
+-- >>> let newMessage = "False alarm!"
+-- >>> view msg (set msg newMessage (ReallyBadError "BAD BAD BAD")) == newMessage
+-- True
+
+-- |
+-- Checking 1st lens law:
+-- You get back what you set (set-get)
+-- >>> let newMessage = "False alarm!"
+-- >>> view msg (set msg newMessage (ExitCode 1))
+-- ""
+
+-- |
+-- >>> let newMessage = "False alarm!"
+-- >>> view msg (set msg newMessage (ExitCode 1)) == newMessage
+-- False
+
+-------------
+-- Session --
+-------------
+type UserName = String
+
+type UserId = String
+
+data Session = Session
+  { _userId ∷ UserId,
+    _userName ∷ UserName,
+    _createdTime ∷ String,
+    _expiryTime ∷ String
+  }
+  deriving (Show, Eq)
+
+makeLenses ''Session
+
+userInfo ∷ Lens' Session (UserId, UserName)
+userInfo = lensProduct userId userName
+
+-- |
+-- userId and userName are disjoint:
+-- >>> let session = Session "USER-1234" "Joey Tribbiani" "2019-07-25" "2019-08-25"
+-- >>> view userInfo session
+-- ("USER-1234","Joey Tribbiani")
+
+-- Using `lensProduct`
+alongsideUserId ∷ Lens' Session (Session, UserId)
+alongsideUserId = lensProduct id userId
+
+-- |
+-- >>> let session = Session "USER-1234" "Joey Tribbiani" "2019-07-25" "2019-08-25"
+-- >>> let newSession = session {_userId = "USER-5678"}
+-- >>> view alongsideUserId (set alongsideUserId (newSession, "USER-9999") session)
+-- (Session {_userId = "USER-9999", _userName = "Joey Tribbiani", _createdTime = "2019-07-25", _expiryTime = "2019-08-25"},"USER-9999")
+
+-- |
+-- 1st lens law
+-- >>> let session = Session "USER-1234" "Joey Tribbiani" "2019-07-25" "2019-08-25"
+-- >>> let newSession = session {_userId = "USER-5678"}
+-- >>> view alongsideUserId (set alongsideUserId (newSession, "USER-9999") session) == (newSession, "USER-9999")
+-- False
+
+-- Using `lensProduct`
+alongsideSession ∷ Lens' Session (UserId, Session)
+alongsideSession = lensProduct userId id
+
+-- |
+-- value in the session overwrites the other
+-- >>> let session = Session "USER-1234" "Joey Tribbiani" "2019-07-25" "2019-08-25"
+-- >>> let newSession = session {_userId = "USER-5678"}
+-- >>> view alongsideSession (set alongsideSession ("USER-9999", newSession) session)
+-- ("USER-5678",Session {_userId = "USER-5678", _userName = "Joey Tribbiani", _createdTime = "2019-07-25", _expiryTime = "2019-08-25"})
+
+-- |
+-- >>> let session = Session "USER-1234" "Joey Tribbiani" "2019-07-25" "2019-08-25"
+-- >>> let newSession = session {_userId = "USER-5678"}
+-- >>> view alongsideSession (set alongsideSession ("USER-9999", newSession) session) == ("USER-9999", newSession)
+-- False
